e22ee8febf284bbafdf9e309cd32febbf08de2a3
Dan Collins <dan.collins.dev@gmail.com>
2025-01-02 21:53:55 -0500

    decided to switch from one liners to easier to read

--- Program.cs
+++ Program.cs
@@ -7,7 +7,9 @@ class Program
         Console.Write("Enter a number: ");
         int number = Convert.ToInt32(Console.ReadLine());
 
-        if (number % 2 == 0) Console.WriteLine("Tick");
-        else Console.WriteLine("Tock");
+        if (number % 2 == 0)
+            Console.WriteLine("Tick");
+        else 
+            Console.WriteLine("Tock");
     }
 }
