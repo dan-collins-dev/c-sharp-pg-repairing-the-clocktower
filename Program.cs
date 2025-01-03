94d096963e064039d73b2320c5c47be0cb65fb45
Dan Collins <dan.collins.dev@gmail.com>
2025-01-02 21:50:41 -0500

    challenge completed

--- Program.cs
+++ Program.cs
@@ -4,6 +4,10 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        Console.Write("Enter a number: ");
+        int number = Convert.ToInt32(Console.ReadLine());
+
+        if (number % 2 == 0) Console.WriteLine("Tick");
+        else Console.WriteLine("Tock");
     }
 }
